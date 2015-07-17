45d8b164b36b3cf0feec9f5c5ab250366c77e05f
rimi <rimi@E0035.local>
2015-07-17 09:33:26 +0800

    cangku111

---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by rimi on 15/7/17.
+//  Copyright (c) 2015年 rimi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
